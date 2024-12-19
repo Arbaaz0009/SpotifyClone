1d4f8e80f6ef2dc285d6c99b71bbeaa47b2aa8b2
Arbaz009 <arbazansari78692s@gmail.com>
2024-12-19 16:13:21 +0530

    Update Header.jsx

--- src/components/navbar/Header.jsx
+++ src/components/navbar/Header.jsx
@@ -6,7 +6,7 @@ import { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import  Profile from '../profile/profile';
-import './header.css';
+import './Header.css';
 export default function Header() {
     const navigate = useNavigate();
     const [searchvalue, setsearchvalue] = useState('');
