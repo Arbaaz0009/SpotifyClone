b3c17aa24948a78f586dac564e10f28a1fd70aa0
Arbaz009 <arbazansari78692s@gmail.com>
2024-12-19 21:01:24 +0530

    Update spotify.js

--- src/spotify.js
+++ src/spotify.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 const authEndPoint = "https://accounts.spotify.com/authorize";
 const ClientId = "37679ad627494fbcbe1ac60fda0992c6";
-const redirectUri = "http://spotify-clone-eight-ochre.vercel.app";
+const redirectUri = " http://localhost:5173";
 const scopes = [
   "playlist-read-private",
   "user-library-modify",
