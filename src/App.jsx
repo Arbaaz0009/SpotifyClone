663edfab69fa3c98bdf6b6d5e01b6b616b4fb7ce
Arbaz009 <arbazansari78692s@gmail.com>
2025-01-20 23:43:19 +0530

    Revert ","
    This reverts commit 9eacbe9e13e2b2e0db39e9798c8ef711fdd5a5ff.

--- src/App.jsx
+++ src/App.jsx
@@ -3,59 +3,48 @@ import {
   RouterProvider,
 } from "react-router-dom";
 import { lazy, Suspense } from "react";
-import "./App.css";
+import './App.css';
 import Loading from "./Pages/Loading/Loading";
 
-// Lazy-load components
 const Home = lazy(() => import("./Pages/Home/Home"));
 const Login = lazy(() => import("./Pages/Auth/Login"));
 const SignUp = lazy(() => import("./Pages/Auth/SignUp"));
 const Playlist = lazy(() => import("./Pages/playlist/playlist"));
 
-// Router configuration
+
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Home />,
-    errorElement: <h1>Error loading the home page</h1>, // Error boundary for the Home route
     children: [
       {
         path: "playlist",
-        element: (
-          <Suspense fallback={<Loading />}>
-            <Playlist />
-          </Suspense>
-        ),
+        element: <Playlist />
       },
-    ],
+    ]
+
   },
   {
     path: "/login",
-    element: (
-      <Suspense fallback={<Loading />}>
-        <Login />
-      </Suspense>
-    ),
+    element: <Login />,
   },
   {
     path: "/signup",
-    element: (
-      <Suspense fallback={<Loading />}>
-        <SignUp />
-      </Suspense>
-    ),
-  },
-  
-]);
+    element: <SignUp />,
+  }
 
+
+
+]);
 function App() {
-  console.log("App.jsx loaded");
+  console.log("app.jsx loaded");
+
 
   return (
-    <Suspense fallback={<Loading />}>
+    <Suspense fallback={<Loading/>}>
       <RouterProvider router={router} />
     </Suspense>
   );
 }
 
-export default App;
+export default App
