6fdb4688fd97fe47a9a1fc405f262c091c3501ba
Arbaz009 <arbazansari78692s@gmail.com>
2025-01-21 21:29:58 +0530

    Update useSpotifyPlayer.jsx

--- src/Hooks/useSpotifyPlayer.jsx
+++ src/Hooks/useSpotifyPlayer.jsx
@@ -4,47 +4,44 @@ import { useState, useEffect } from 'react';
 const useSpotifyPlayer = (token) => {
   const [player, setPlayer] = useState(null);
   const [deviceId, setDeviceId] = useState(null);
-  console.log("hook loaded");
-  
+
   useEffect(() => {
     const script = document.createElement('script');
     script.src = 'https://sdk.scdn.co/spotify-player.js';
     script.async = true;
     document.body.appendChild(script);
 
-    script.onload = () => {
-      window.onSpotifyWebPlaybackSDKReady = () => {
-        const player = new window.Spotify.Player({
-          name: 'SpotifyClone Player',
-          getOAuthToken: cb => { cb(token); },
-          volume: 0.5
-        });
+    window.onSpotifyWebPlaybackSDKReady = () => {
+      const player = new window.Spotify.Player({
+        name: 'SpotifyClone Player',
+        getOAuthToken: cb => { cb(token); },
+        volume: 0.5
+      });
 
-        // Error handling
-        player.addListener('initialization_error', ({ message }) => { console.error(message); });
-        player.addListener('authentication_error', ({ message }) => { console.error(message); });
-        player.addListener('account_error', ({ message }) => { console.error(message); });
-        player.addListener('playback_error', ({ message }) => { console.error(message); });
+      // Error handling
+      player.addListener('initialization_error', ({ message }) => { console.error(message); });
+      player.addListener('authentication_error', ({ message }) => { console.error(message); });
+      player.addListener('account_error', ({ message }) => { console.error(message); });
+      player.addListener('playback_error', ({ message }) => { console.error(message); });
 
-        // Playback status updates
-        player.addListener('player_state_changed', state => {
-          console.log(state);
-        });
+      // Playback status updates
+      player.addListener('player_state_changed', state => {
+        console.log(state);
+      });
 
-        // Ready
-        player.addListener('ready', ({ device_id }) => {
-          console.log('Ready with Device ID', device_id);
-          setDeviceId(device_id);
-        });
+      // Ready
+      player.addListener('ready', ({ device_id }) => {
+        console.log('Ready with Device ID', device_id);
+        setDeviceId(device_id);
+      });
 
-        // Not Ready
-        player.addListener('not_ready', ({ device_id }) => {
-          console.log('Device ID has gone offline', device_id);
-        });
+      // Not Ready
+      player.addListener('not_ready', ({ device_id }) => {
+        console.log('Device ID has gone offline', device_id);
+      });
 
-        player.connect();
-        setPlayer(player);
-      };
+      player.connect();
+      setPlayer(player);
     };
 
     return () => {
